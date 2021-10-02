373f34a260a12cc4d7f5b012c241a2fa8f42b57a
Steve Xing <stevethesteve990@gmail.com>
2021-10-02 19:23:09 -0400

    init

---
+++ functions/src/exportUsers.ts
@@ -0,0 +1,45 @@
+// Google Sheet
+import { google } from 'googleapis'
+const sheets = google.sheets('v4')
+
+const serviceAccount = require('../sheets_updater_service_account.json')
+
+const jwtClient = new google.auth.JWT({
+    email: serviceAccount.client_email,
+    key: serviceAccount.private_key,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+})
+
+const jwtAuthPromise = jwtClient.authorize()
+
+export async function exportUsers(
+    date: string,
+    fullName: string,
+    isFullyLoggedIn: boolean,
+    shirtSize: string,
+    school: string,
+    specialAccomadations: string,
+    phone: string,
+    graduationYear: string,
+    latino: boolean,
+    ethnicity: string, 
+    gender: string,
+    studyLevel: string
+) {
+
+    const finalData: Array<Array<string>> = [];
+    const singleUser: Array<any> = [date, fullName, isFullyLoggedIn, shirtSize, school,
+                                    specialAccomadations, phone, graduationYear,
+                                    latino, ethnicity, gender, studyLevel];
+    finalData.push(singleUser);
+
+    await jwtAuthPromise
+    await sheets.spreadsheets.values.append({
+        auth: jwtClient,
+        spreadsheetId: "12Du6pww7wz6NHZQATMJqGrU7pViuPooNhUqtZYXGdpk",
+        range: `Sheet1!A1:Z1`,
+        valueInputOption: 'RAW',
+        requestBody: { values: finalData, majorDimension: "ROWS" }
+    }, {})
+
+}
---
+++ functions/src/index.ts
@@ -0,0 +1,30 @@
+import * as functions from "firebase-functions";
+import { exportUsers } from "./exportUsers";
+
+const region = "us-central1"
+
+exports.exportOrderData = functions.region(region).firestore.document('users/{id}').onCreate(async (snapshot, _) => {
+    const docData = snapshot.data();
+
+    const date = docData.dateCreated.toDate().toLocaleString('en-US', { datestyle: 'long', timestyle: 'long'});
+    const fullName = docData.fullName; 
+    const isFullyLoggedIn = docData.isFullyLoggedIn;
+    const shirtSize = docData.shirtSize;
+    const school = docData.school;
+    const specialAccomadations = docData.specialAccomadations;
+    const phone = docData.phone;
+    const graduationYear = docData.graduationYear;
+    const latino = docData.latino;
+    const ethnicity = docData.ethnicity;
+    const gender = docData.gender;
+    
+    //let githubLink = "";
+    //if(docData.githubLink) githubLink = docData.githubLink;
+    
+    const studyLevel = docData.studyLevel;
+    
+
+    await exportUsers(date, fullName, isFullyLoggedIn, shirtSize, school,
+                      specialAccomadations, phone, graduationYear,
+                      latino, ethnicity, gender, studyLevel);
+});
